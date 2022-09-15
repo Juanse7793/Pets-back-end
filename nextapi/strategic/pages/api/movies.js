eeac696976956f8018600dff01f69b28071a08ff
Juan Sebastian <juanse.sotomayor77@gmail.com>
2022-09-15 14:48:18 -0500

    deploy changes

--- nextapi/strategic/pages/api/movies.js
+++ nextapi/strategic/pages/api/movies.js
@@ -1,4 +1,4 @@
-import { connectToDatabase } from '../util/mongodb';
+import { connectToDatabase } from '../../components/util/mongodb';
 
 export default async (req, res) => {
   const { db } = await connectToDatabase();
